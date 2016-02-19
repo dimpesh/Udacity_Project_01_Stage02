0f66d50a383c8012788e6cd7caa46b7e4d2c2887
dk_2094 <kdimpesh@gmail.com>
2016-02-19 20:01:35 +0530

    initial commit

---
+++ app/src/main/java/com/movies/app/popularmovies/DetailActivityFragment.java
@@ -0,0 +1,293 @@
+package com.movies.app.popularmovies;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+    String baseUrlImage="http://image.tmdb.org/t/p/w185/";
+    TrailerAdapter myTrailerAdapter;
+    TrailerObject[] trailerObjects;
+    List<String> trailerKeyList;
+    String LOG_TAG= DetailActivityFragment.class.getSimpleName();
+    String MyAppString = "#SpotifyStreamer";
+    String reviewStr;
+    String trailer_string;
+    int cnt=0;
+    TextView reviewView;
+    public DetailActivityFragment() {
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView=inflater.inflate(R.layout.fragment_detail, container, false);
+        Intent intent=getActivity().getIntent();
+//l4        Bundle bundle= intent.getExtras();
+        String API_KEY="api_key";
+   //     trailerAdapter=new TrailerAdapter(getActivity(),R.layout.list_item_trailer,R.id.trailer_image,trailerKeyList);
+        FloatingActionButton updateFavouriteButton= (FloatingActionButton) rootView.findViewById(R.id.favbtn);
+        // Trailer Object Populate the Trailer ListView
+        trailerObjects= new TrailerObject[]{new TrailerObject(), new TrailerObject()};
+
+        List<TrailerObject> listmovie=new ArrayList<TrailerObject>(Arrays.asList(trailerObjects));
+        myTrailerAdapter=new TrailerAdapter(getActivity(),R.layout.trailer_item_imageview,R.id.gridview_movies_imageview,listmovie);
+
+
+//l4        MovieObject movieRecieved= (MovieObject) bundle.getSerializable("MovieObjectSent");
+            MovieObject movieRecieved=intent.getParcelableExtra("data");
+        ImageView movieBackdrop= (ImageView) rootView.findViewById(R.id.movie_backdrop);
+        ImageView moviePoster= (ImageView) rootView.findViewById(R.id.movie_poster);
+        TextView movie_vote_average= (TextView) rootView.findViewById(R.id.movie_vote_average);
+        TextView movie_release= (TextView) rootView.findViewById(R.id.movie_release);
+        Picasso.with(getContext()).load(baseUrlImage+movieRecieved.backdrop_path).into(movieBackdrop);
+        Picasso.with(getContext()).load(baseUrlImage+movieRecieved.poster_path).into(moviePoster);
+        getActivity().setTitle(movieRecieved.title);
+        new DataFetcher().execute(movieRecieved.id);
+       // new TrailerFetcher().execute(movieRecieved.id);
+   //     new TrailerFetcher().execute(movieRecieved.id);
+
+        GridView trailerview= (GridView) rootView.findViewById(R.id.gridview_trailersview);
+        trailerview.setAdapter(myTrailerAdapter);
+
+
+        trailerview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                TrailerObject trailerObject=myTrailerAdapter.getItem(position);
+                String key=trailerObject.getKey();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=" + key)));
+            }
+        });
+
+        TextView overview= (TextView) rootView.findViewById(R.id.movie_overview);
+
+        overview.setText(movieRecieved.overview);
+
+        movie_release.setText(movieRecieved.release_date);
+        movie_vote_average.setText(movieRecieved.vote_average);
+        reviewView= (TextView) rootView.findViewById(R.id.movie_review);
+      //  reviewView.setText(reviewStr);
+
+
+        return rootView;
+
+
+    }
+
+    public class DataFetcher extends AsyncTask<String,Void,String>
+    {
+        ProgressDialog dialog=new ProgressDialog(getActivity());
+        String LOG_TAG= DataFetcher.class.getSimpleName();
+        String API_KEY="api_key";
+        String jsonStr;
+        int cnt=1;
+        String reviewStr="";
+        String movie_id;
+        BufferedReader reader=null;
+        // String baseUrl="https://api.themoviedb.org/3/movie/286217/";
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Loading...");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String str)
+        {
+
+            reviewView.setText(str);
+            new TrailerFetcher().execute(movie_id);
+
+            if(dialog.isShowing())
+                dialog.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(String...strings) {
+            //Uri buildUrl=Uri.parse(baseUrl+strings[0]).buildUpon().appendQueryParameter(API_KEY, getString(R.string.api_key)).build();
+            try {
+                movie_id=strings[0];
+                URL url=new URL("https://api.themoviedb.org/3/movie/"+strings[0]+"/reviews?api_key=69323240f26aaa3f0ed513e2fd344a5f");
+                Log.v(LOG_TAG+"VERBOSE",url.toString());
+                urlConnection=(HttpURLConnection)url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream=urlConnection.getInputStream();
+                StringBuffer buffer=new StringBuffer();
+                if(inputStream==null)
+                    jsonStr=null;
+
+                reader=new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line=reader.readLine())!=null)
+                {
+                    buffer.append(line+"\n");
+                }
+                if(buffer.length()==0)
+                {
+                    jsonStr=null;
+                }
+                jsonStr=buffer.toString();
+                JSONObject jsonObject=new JSONObject(jsonStr);
+                JSONArray jsonArray =jsonObject.getJSONArray("results");
+                for(int i=0;i<jsonArray.length();i++) {
+                    JSONObject review = jsonArray.getJSONObject(i);
+                    //JSONObject authorObject=review.getJSONObject("author");
+                    String authorName=review.getString("author");
+                    Log.v("\nauthor JSON",authorName);
+                    //JSONObject contentObject=review.getJSONObject("content");
+                    String content=review.getString("content").toString();
+                    Log.v("\ncontent JSON ",authorName);
+                    reviewStr=reviewStr+"\n \t\t:: REVIEW "+cnt+" ::\n\n"+authorName+"\n"+content+"\n";
+                    cnt++;
+                }
+                Log.v(LOG_TAG,reviewStr);
+
+
+
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return reviewStr;
+
+
+
+        }
+    }
+
+    public class TrailerFetcher extends AsyncTask<String, Void, TrailerObject[]> {
+        ProgressDialog dialog=new ProgressDialog(getActivity());
+
+        String LOG_TAG = TrailerFetcher.class.getSimpleName();
+        String API_KEY = "api_key";
+        String jsonStr;
+        String myTrailer = "";
+        //        String []trailer_key=null;
+        BufferedReader reader = null;
+        // String baseUrl="https://api.themoviedb.org/3/movie/286217/";
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected void onPostExecute(TrailerObject[] str) {
+
+            if (str != null)
+                myTrailerAdapter.clear();
+
+            myTrailerAdapter.addAll(str);
+            myTrailerAdapter.notifyDataSetChanged();
+
+            if(dialog.isShowing()==true)
+            {
+                dialog.dismiss();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Loading...");
+            dialog.show();
+        }
+
+        @Override
+        protected TrailerObject[] doInBackground(String... strings) {
+            try {
+                URL url = new URL("https://api.themoviedb.org/3/movie/"+strings[0]+"/videos?api_key=69323240f26aaa3f0ed513e2fd344a5f");
+                Log.v(LOG_TAG + "VERBOSE", url.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null)
+                    jsonStr = null;
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    jsonStr = null;
+                }
+                jsonStr = buffer.toString();
+                JSONObject trailerObject = new JSONObject(jsonStr);
+                Log.v("VERBOSE",trailerObject.toString());
+                JSONArray jsonArray = trailerObject.getJSONArray("results");
+                trailerObjects = new TrailerObject[jsonArray.length()];
+                for (int i = 0; i < jsonArray.length(); i++)
+                {
+                    trailerObjects[i] = new TrailerObject();
+                    JSONObject jsonArrayJSONObject = jsonArray.getJSONObject(i);
+                    //JSONObject authorObject=review.getJSONObject("author");
+                    trailerObjects[i].id = jsonArrayJSONObject.getString("id");
+                    Log.v("ID VERBOSE",jsonArrayJSONObject.getString("key"));
+                    trailerObjects[i].key = jsonArrayJSONObject.getString("key");
+                    trailerObjects[i].name = jsonArrayJSONObject.getString("name");
+
+
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return trailerObjects;
+        }
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/movies/app/popularmovies/MainActivityFragment.java
@@ -0,0 +1,229 @@
+package com.movies.app.popularmovies;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.ContentLoadingProgressBar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+    String popular="popularity.desc";
+    String top="vote_average.desc";
+    List<MovieObject>listmovie;
+     MovieObject []movieObjects;
+    private MovieAdapter movieAdapter;
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//saved instance
+//        if(savedInstanceState==null || !savedInstanceState.containsKey("movies"))
+//        {
+//            listmovie=new ArrayList<MovieObject>()
+//        }
+//    //    setRetainInstance(true);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+     //   super.onCreateOptionsMenu(menu, inflater);
+       inflater.inflate(R.menu.fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.action_popular)
+        {
+            new FetchMovieTask().execute(popular);
+            return true;
+
+        }
+        if(id==R.id.action_top_rated)
+        {
+            new FetchMovieTask().execute(top);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /// Adding save Instance State...
+//    @Override
+//    public void onSaveInstanceState(Bundle outState) {
+//        outState.putParcelableArray("movies",listmovie);
+//        super.onSaveInstanceState(outState);
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //String []moviesArray= {"MONDAY","TUESDAY","WENESDAY","THURSDAY"};
+        movieObjects= new MovieObject[]{new MovieObject("a"), new MovieObject("b")};
+        //List<String>listmovie=new ArrayList<String>(Arrays.asList(moviesArray));
+        listmovie=new ArrayList<MovieObject>(Arrays.asList(movieObjects));
+        //movieAdapter=new MovieAdapter(getActivity(),R.layout.grid_item_movies,R.id.grid_item_movies_imageview,listmovie);
+        movieAdapter=new MovieAdapter(getActivity(),R.layout.grid_item_movies,R.id.grid_item_movies_imageview,listmovie);
+        View rootview=inflater.inflate(R.layout.fragment_main,container,false);
+        new FetchMovieTask().execute("popularity.desc");
+        GridView gridView= (GridView) rootview.findViewById(R.id.gridview_movies);
+
+        gridView.setAdapter(movieAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+            MovieObject movieClicked= (MovieObject) movieAdapter.getItem(position);
+                //String overview=movieClicked.overview;
+                //Toast.makeText(getActivity(),overview,Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(getActivity(),DetailActivity.class);
+//l4                Bundle mBundle=new Bundle();
+
+//                mBundle.putSerializable("MovieObjectSent",movieClicked);
+//                intent.putExtras(mBundle);
+                intent.putExtra("data",movieClicked);
+                startActivity(intent);
+
+
+            }
+        });
+        return rootview;
+        //return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+    class FetchMovieTask extends AsyncTask<String,Void,MovieObject[]>
+    {
+        ProgressDialog dialog=new ProgressDialog(getActivity());
+        ContentLoadingProgressBar progressBar=new ContentLoadingProgressBar(getContext());
+        MovieObject [] movieObjects=null;
+   //     String []str=null;
+
+        @Override
+        protected void onPostExecute(MovieObject[] str) {
+        if(str!=null)
+                movieAdapter.clear();
+
+            for(MovieObject m : str)
+            {
+                movieAdapter.add(m);
+            }
+
+            if(dialog.isShowing()==true)
+            {
+                dialog.dismiss();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Loading...");
+            dialog.show();
+
+        }
+
+        @Override
+        protected MovieObject[] doInBackground(String... strings) {
+            final String movieBaseUrl="http://api.themoviedb.org/3/discover/movie?";
+            String api_key=getString(R.string.api_key);
+            String API_PARAM="api_key";
+            String TYPE_PARAM="sort_by";
+            String YEAR_PARAM="primary_release_year";
+            String year="2015";
+            String type=strings[0];
+            String TYPE_VOTE_COUNT="vote_count.gte";
+            String voteCount="50";
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String movieJSONStr = null;
+            try {
+
+                Uri buildUri=Uri.parse(movieBaseUrl).buildUpon().appendQueryParameter(YEAR_PARAM,year).appendQueryParameter(TYPE_VOTE_COUNT,voteCount).appendQueryParameter(TYPE_PARAM,type).appendQueryParameter("vote_count","1000").appendQueryParameter(API_PARAM,api_key).build();
+                Log.v("URL",buildUri.toString());
+                URL url=new URL(buildUri.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    movieJSONStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    movieJSONStr = null;
+                }
+                movieJSONStr = buffer.toString();
+                String title="title";
+                String vote_average="vote_avergae";
+                String overview="overview";
+                JSONObject movieJSONObject=new JSONObject(movieJSONStr);
+                JSONArray movieJSONArray=movieJSONObject.optJSONArray("results");
+                movieObjects=new MovieObject[movieJSONArray.length()];
+                for(int i=0;i<movieJSONArray.length();i++)
+                {
+                    movieObjects[i]=new MovieObject();
+                    JSONObject jsonObject=movieJSONArray.getJSONObject(i);
+                    movieObjects[i].title=jsonObject.optString("title").toString();
+                    movieObjects[i].overview=jsonObject.optString("overview").toString();
+                    movieObjects[i].poster_path=jsonObject.optString("poster_path").toString();
+                    movieObjects[i].release_date = jsonObject.getString("release_date").toString();
+                    movieObjects[i].vote_average=jsonObject.getString("vote_average").toString();
+                    movieObjects[i].backdrop_path=jsonObject.getString("backdrop_path").toString();
+                    movieObjects[i].id=jsonObject.getString("id").toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return movieObjects;
+
+
+        }
+    }
+}
